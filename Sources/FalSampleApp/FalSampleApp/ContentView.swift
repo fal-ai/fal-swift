98033482e918669f834372eef2a77202a757f8c2
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-11-05 01:33:30 -0800

    chore: add sample app

---
+++ Sources/FalSampleApp/FalSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FalSampleApp
+//
+//  Created by Daniel Rochetti on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sources/FalSampleApp/FalSampleApp/FalSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalSampleAppApp.swift
+//  FalSampleApp
+//
+//  Created by Daniel Rochetti on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FalSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
